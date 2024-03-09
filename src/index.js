eabde6c30a65d2d23dc7e2ee321c3ae11ff46d41
d3monw1ngs <tezzielumsit87@gmail.com>
2024-03-09 16:38:28 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
-const breedSelectElement = document.querySelector('.breed-select');
-const catInfoElement = document.querySelector('.cat-info');
-const loaderElement = document.querySelector('.loader');
-const errorElement = document.querySelector('.error');
+const breedSelectEl = document.querySelector(".breed-select");
+const catInfoEl = document.querySelector(".cat-info");
+const loaderEl = document.querySelector(".loader");
+const errorEl = document.querySelector(".error");
 
-function selectBreed() {
-    fetchBreeds(data).then((data) => {
+errorEl.classList.add("is-hidden");
 
-        let optionsMarkup = data.map(({ name, id }) => {
-            return `<option value=${id}>${name}</option>`;
+function chooseBreed() {
+    fetchBreeds()
+        .then((data) => {
+            loaderEl.classList.replace("loader", "is-hidden");
+
+            let optionsMarkup = data.map(({ name, id }) => {
+                return `<option value=${id}>${name}</option>`;
         });
-        breedSelectElement.insertAdjacentElement("beforeend", optionsMarkup);
-
+        breedSelectEl.insertAdjacentHTML("beforeend", optionsMarkup);
+        breedSelectEl.classList.remove("is-hidden");
     })
+        .catch(onError);
 }
+
+chooseBreed();
+
+breedSelectEl.addEventListener("change", (e) => {
+
+    loaderEl.classList.replace("is-hidden", "loader");
+
+    catInfoEl.classList.add("is-hidden");
+
+    let breedId = e.target.value;
+
+    fetchCatByBreed(breedId).then((data) => {
+        const { url, breeds } = data[0]
+        const { name, description, temparament } = breeds[0];
+
+        catInfoEl.innerHTML = `
+            <img src="${url}" alt='{name}' width="400"/>
+            <div class="box">
+                <h2>${name}</h2>
+                <p>${description}</p>
+                <p>${temparament}</p>
+            </div>
+        `;
+        catInfoEl.classList.remove("is-hidden");
+        loaderEl.classList.add("is-hidden");
+    })
+    .catch(onError);
+});
+
+
+function onError() {
+    errorEl.classList.remove("is-hidden");
+    breedSelectEl.classList.add("is-hidden");
+}
+    
